d4e2406394264663b41dcb55e36215787ff993e8
wardogordo <wardh@me.com>
2022-05-11 12:00:25 -0500

    Solved and completed in C++.

--- your-ride-is-here-cpp/main.cpp
+++ your-ride-is-here-cpp/main.cpp
@@ -1,17 +1,55 @@
 #include <iostream>
 #include <string>
 
-int main()
+std::string cometName{};
+std::string groupName{};
+int cometProduct{1};
+int groupProduct{1};
+
+void ComputeComet()
 {
 
+    for (int i = 0; i < cometName.length(); ++i)
+    {
+        cometProduct *= (int)cometName[i] - 64;
+    }
+
+    std::cout << "Comet total: " << cometProduct << '\n';
+}
+
+void ComputeGroup()
+{
+
+    for (int i = 0; i < groupName.length(); ++i)
+    {
+        groupProduct *= (int)groupName[i] - 64;
+    }
+
+    std::cout << "Group total: " << groupProduct << '\n';
+}
+
+void StayOrGo()
+{
+    std::cout << "cometProduct % 47 = " << cometProduct % 47 << '\n';
+    std::cout << "groupProduct % 47 = " << groupProduct % 47 << '\n';
+    if (cometProduct % 47 == groupProduct % 47)
+        std::cout << "GO" << '\n';
+    else
+        std::cout << "STAY" << '\n';
+}
+
+int main()
+{
     std::cout << "Enter a comet name: ";
-    std::string cometName{};
     std::cin >> cometName;
     std::cout << "Now enter a group name: ";
-    std::string groupName{};
     std::cin >> groupName;
     std::cout << "Comet: " << cometName << '\n';
     std::cout << "Group: " << groupName << '\n';
 
+    ComputeComet();
+    ComputeGroup();
+    StayOrGo();
     return 0;
 }
+
