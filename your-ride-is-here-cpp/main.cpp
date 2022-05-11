ff3b30fbbe4b3178108885a8487a28fd390ede9f
wardogordo <wardh@me.com>
2022-05-11 09:28:33 -0500

    Initial commit.

--- your-ride-is-here-cpp/main.cpp
+++ your-ride-is-here-cpp/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+
+    std::cout << "Enter a comet name: ";
+    std::string cometName{};
+    std::cin >> cometName;
+    std::cout << "Now enter a group name: ";
+    std::string groupName{};
+    std::cin >> groupName;
+    std::cout << "Comet: " << cometName << '\n';
+    std::cout << "Group: " << groupName << '\n';
+
     return 0;
 }
