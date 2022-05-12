87e803254d6adb085bc239e44591a4366e70c513
wardogordo <wardh@me.com>
2022-05-12 10:43:37 -0500

    Input validation for alpha and length completed.

--- your-ride-is-here/Program.cs
+++ your-ride-is-here/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace RideIsHere
 {
@@ -6,20 +8,66 @@ namespace RideIsHere
     {
         static double cometTotal = 1;
         private static double groupTotal = 1;
+        private static string cometName;
+        private static string groupName;
+        private static bool cometNameValidated;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a comet name: ");
-            string cometName = Console.ReadLine();
+            EnterCometName();
             cometName = cometName.ToUpper();
-            Console.WriteLine("Now enter a group name: ");
-            string groupName = Console.ReadLine();
+            cometNameValidated = true;
+            EnterGroupName();
             groupName = groupName.ToUpper();
-            Console.WriteLine(cometName + " " + groupName);
-            
+            Console.WriteLine("Your input: " + cometName + " " + groupName);
             CometProductOfLetters(cometName);
             GroupProductOfLetters(groupName);
             StayOrGo();
+        }
+
+        static void EnterCometName()
+        {
+            Console.WriteLine("Enter a comet name: ");
+            cometName = Console.ReadLine();
+            ValidateInput(cometName);
+        }
+        
+        static void EnterGroupName()
+        {
+            Console.WriteLine("Now enter a group name: ");
+            groupName = Console.ReadLine();
+            ValidateInput(groupName);
+        }
+         static void ValidateInput(string str)
+        {
+            if (!Regex.IsMatch(str, @"^[a-zA-Z]+$"))
+            {
+                Console.WriteLine("Please only use standard alphabet characters.");
+                Console.WriteLine ("Your entry: " + str);
+                if (cometNameValidated)
+                {
+                    EnterGroupName();
+                }
+                else
+                {
+                    EnterCometName();
+                }
+                
+            }
+
+            if (str.Length != 6)
+            {
+                Console.WriteLine("Please enter a 6 character name.");
+                Console.WriteLine ("Your " + str.Length + " character entry: " + str);
+                if (cometNameValidated)
+                {
+                    EnterGroupName();
+                }
+                else
+                {
+                    EnterCometName();
+                }
+            }
         }
         static void CometProductOfLetters(string str)
         {
@@ -45,8 +93,8 @@ namespace RideIsHere
 
         static void StayOrGo()
         {
-            Console.WriteLine(cometTotal % 47);
-            Console.WriteLine(groupTotal % 47);
+            Console.WriteLine("\tComet Total % 47: " + cometTotal % 47);
+            Console.WriteLine("\tGroup Total % 47: " + groupTotal % 47);
             
             if (cometTotal % 47 == groupTotal % 47)
             {
